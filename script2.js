40a48710f2c135f12332779ecdebdee145f7e8cd
aaroncoden187-png <aaroncoden187@gmail.com>
2026-05-13 16:57:14 +0845

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,4 @@
+$name="Aaron Coden"
+document.getElementById("text").innerHTML=$name;
+console.log($name);
+alert($name);
---
+++ script4.js
@@ -0,0 +1,14 @@
+function getInnerText() {
+    let content = document.getElementById("example").innerText;
+    alert(content);
+}
+
+function getInnerHTML() {
+    let content = document.getElementById("example").innerHTML;
+    alert(content);
+}
+
+function getTextContent() {
+    let content = document.getElementById("example").textContent;
+    alert(content);
+}
